f0fa6e5739ca6c5d6e9106b410542416029c3cbb
token <239573049@qq.com>
2024-04-15 13:41:10 +0800

    修改github地址

--- web/src/config.ts
+++ web/src/config.ts
@@ -3,5 +3,5 @@ export const config = {
   FAST_API_URL: import.meta.env.VITE_API_URL ?? "",
   NODE_ENV: import.meta.env.MODE,
   DEV: import.meta.env.DEV,
-  VITE_GITHUB: import.meta.env.VITE_GITHUB ?? "https://gitee.com/hejiale010426/token-api",
+  VITE_GITHUB: import.meta.env.VITE_GITHUB ?? "https://github.com/AIDotNet/AIDotNet.API",
 };
