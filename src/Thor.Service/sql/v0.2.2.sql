1f2fc487f288f87952fb78ddbf7c7f0e5e45f445
token <239573049@qq.com>
2024-10-26 02:14:13 +0800

    增加版本迁移sql

---
+++ src/Thor.Service/sql/v0.2.2.sql
@@ -0,0 +1,10 @@
+﻿alter table Tokens
+    add LimitModels TEXT not null;
+
+alter table Tokens
+    add WhiteIpList TEXT not null;
+
+-- 需要给 表提供默认值
+update from Tokens
+set limitModels = '[]', WhiteIpList = '[]'
+where limitModels is null or WhiteIpList is null;
