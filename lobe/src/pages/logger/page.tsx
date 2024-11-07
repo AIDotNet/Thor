d9702f79361332430f416e758a89dacbd20bc49e
赵畅畅 <zhaocc@1391.com>
2024-11-07 16:25:53 +0800

    消费日志表格宽度调整

--- lobe/src/pages/logger/page.tsx
+++ lobe/src/pages/logger/page.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components"
-import { message, Dropdown, Button, Input, Select, DatePicker, Table, Card } from "antd";
+import styled from "styled-components";
+import {
+  message,
+  Dropdown,
+  Button,
+  Input,
+  Select,
+  DatePicker,
+  Table,
+  Card,
+} from "antd";
 import { getLoggers, viewConsumption } from "../../services/LoggerService";
 import { Tag } from "@lobehub/ui";
 import { renderQuota } from "../../utils/render";
 
-
-const Header = styled.header`
-
-`
+const Header = styled.header``;
 
 export default function LoggerPage() {
   const [data, setData] = useState<any[]>([]);
@@ -20,10 +26,10 @@ export default function LoggerPage() {
     page: 1,
     pageSize: 10,
     type: -1 | 1 | 2 | 3,
-    model: '',
-    startTime: '',
-    endTime: '',
-    keyword: ''
+    model: "",
+    startTime: "",
+    endTime: "",
+    keyword: "",
   });
 
   function timeString(totalTime: number) {
@@ -31,195 +37,238 @@ export default function LoggerPage() {
     let s = totalTime / 1000;
     let m = Math.floor(s / 60);
     s = Math.floor(s % 60);
-    return `${m}分${s}秒`
+    return `${m}分${s}秒`;
   }
 
   const columns = [
     {
-      title: '时间',
-      dataIndex: 'createdAt',
-      key: 'createdAt'
+      title: "时间",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      width: "180px",
     },
     {
-      disable: !(localStorage.getItem('role') === "admin"),
-      title: '渠道',
-      dataIndex: 'channelName',
-      width: '80px',
-      key: 'channelName',
+      disable: !(localStorage.getItem("role") === "admin"),
+      title: "渠道",
+      dataIndex: "channelName",
+      width: "190px",
+      key: "channelName",
       render: (value: any) => {
-        return value && <Tag color='blue'>{value}</Tag>
-      }
+        return value && <Tag color="blue">{value}</Tag>;
+      },
     },
     {
-      title: '用户',
-      dataIndex: 'userName',
-      width: '80px',
-      key: 'userName',
+      title: "用户",
+      dataIndex: "userName",
+      width: "90px",
+      key: "userName",
       render: (value: any) => {
-        return value && <Tag color='blue'>{value}</Tag>
-      }
+        return value && <Tag color="blue">{value}</Tag>;
+      },
     },
     {
-      title: '模型',
-      dataIndex: 'modelName',
-      width: '180px',
-      key: 'modelName',
+      title: "令牌名称",
+      dataIndex: "tokenName",
+      width: "190px",
+      key: "tokenName",
+    },
+    {
+      title: "模型",
+      dataIndex: "modelName",
+      width: "180px",
+      key: "modelName",
       render: (value: any) => {
-        return value && <Tag onClick={() => {
-          navigator.clipboard.writeText(value).then(() => {
-            message.success({
-              content: '复制成功',
-            })
-          }).catch(() => {
-            message.error({
-              content: '复制失败',
-            })
-          });
-        }}>{value}</Tag>
-      }
+        return (
+          value && (
+            <Tag
+              onClick={() => {
+                navigator.clipboard
+                  .writeText(value)
+                  .then(() => {
+                    message.success({
+                      content: "复制成功",
+                    });
+                  })
+                  .catch(() => {
+                    message.error({
+                      content: "复制失败",
+                    });
+                  });
+              }}
+            >
+              {value}
+            </Tag>
+          )
+        );
+      },
     },
     {
-      title: '用时',
-      dataIndex: 'duration',
-      key: 'duration',
+      title: "用时",
+      dataIndex: "duration",
+      key: "duration",
+      width: "120px",
       render: (_: any, item: any) => {
-        return (<>
-          <Tag color='pink'>{timeString(item.totalTime)}ms</Tag>
-          <Tag color='gold'>
-            {item.stream ? '流式' : '非流式'}
-          </Tag>
-        </>)
-      }
+        return (
+          <>
+            <Tag color="pink">{timeString(item.totalTime)}ms</Tag>
+            <Tag color="gold">{item.stream ? "流式" : "非流式"}</Tag>
+          </>
+        );
+      },
     },
     {
-      title: '提示tokens',
-      dataIndex: 'promptTokens',
-      key: 'promptTokens'
+      title: "提示tokens",
+      dataIndex: "promptTokens",
+      key: "promptTokens",
+      width: "120px",
     },
     {
-      title: '完成tokens',
-      dataIndex: 'completionTokens',
-      key: 'completionTokens'
+      title: "完成tokens",
+      dataIndex: "completionTokens",
+      key: "completionTokens",
+      width: "120px",
     },
     {
-      title: '请求IP地址',
-      dataIndex: 'ip',
-      key: 'ip'
+      title: "请求IP地址",
+      dataIndex: "ip",
+      key: "ip",
+      width: "120px",
     },
     {
-      title: '客户端信息',
-      dataIndex: 'userAgent',
-      width: '80px',
-      key: 'userAgent'
-    },
-    {
-      title: '额度',
-      dataIndex: 'quota',
-      key: 'quota',
+      title: "额度",
+      dataIndex: "quota",
+      key: "quota",
+      width: "120px",
       render: (value: any) => {
-        return value && <Tag color='green'>{renderQuota(value, 6)}</Tag>
-      }
+        return value && <Tag color="green">{renderQuota(value, 6)}</Tag>;
+      },
     },
     {
-      title: '详情',
-      dataIndex: 'content',
-      key: 'content'
-    }
-  ]
+      title: "详情",
+      dataIndex: "content",
+      key: "content",
+      width: "250px",
+    },
+    {
+      title: "客户端信息",
+      dataIndex: "userAgent",
+      width: "380px",
+      key: "userAgent",
+    },
+  ];
 
   function loadData() {
     setLoading(true);
 
     getLoggers(input)
-      .then(res => {
+      .then((res) => {
         if (res.success) {
           setData(res.data.items);
           setTotal(res.data.total);
         } else {
           message.error({
-            content: res.message
+            content: res.message,
           });
         }
       })
       .finally(() => {
         setLoading(false);
-      })
+      });
   }
 
   function loadViewConsumption() {
     setConsumeLoading(true);
     viewConsumption(input)
-      .then(res => {
+      .then((res) => {
         if (res.success) {
           setConsume(res.data);
         } else {
           message.error({
-            content: res.message
+            content: res.message,
           });
         }
-      }).finally(() => {
-        setConsumeLoading(false)
       })
+      .finally(() => {
+        setConsumeLoading(false);
+      });
   }
 
   useEffect(() => {
-    loadData()
+    loadData();
   }, [input.page, input.pageSize]);
 
   return (
-    <div style={{
-      margin: '10px',
-      height: '80vh',
-      overflow: 'auto',
-      width: '100%',
-    }}>
+    <div
+      style={{
+        margin: "10px",
+        height: "80vh",
+        overflow: "auto",
+        width: "100%",
+      }}
+    >
       <Header>
-        <Card style={{
-          float: 'left',
-        }}
+        <Card
+          style={{
+            float: "left",
+          }}
           bodyStyle={{
-            padding: '8px',
-          }}>
+            padding: "8px",
+          }}
+        >
           <span>
-            区间消费: <span style={{
-              fontWeight: 'bold',
-            }}>{renderQuota(consume, 4)}</span>
+            区间消费:{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              {renderQuota(consume, 4)}
+            </span>
           </span>
 
           <Button
             type="text"
             onClick={() => {
-              loadViewConsumption()
+              loadViewConsumption();
             }}
             loading={consumeLoading}
             style={{
-              marginLeft: '0.5rem',
-            }}>
+              marginLeft: "0.5rem",
+            }}
+          >
             查看消费
           </Button>
-
         </Card>
         <Dropdown
           menu={{
             items: [
               {
                 key: 1,
-                label: '删除选中令牌',
+                label: "删除选中令牌",
                 onClick: () => {
-                  console.log('删除选中令牌')
-                }
-              }
-            ]
+                  console.log("删除选中令牌");
+                },
+              },
+            ],
           }}
         >
-          <Button style={{
-            float: 'right',
-          }}>操作</Button>
+          <Button
+            style={{
+              float: "right",
+            }}
+          >
+            操作
+          </Button>
         </Dropdown>
-        <Button onClick={() => loadData()} style={{
-          marginRight: '0.5rem',
-          float: 'right',
-        }}>搜索</Button>
+        <Button
+          onClick={() => loadData()}
+          style={{
+            marginRight: "0.5rem",
+            float: "right",
+          }}
+        >
+          搜索
+        </Button>
         <Input
           value={input.model}
           onChange={(e) => {
@@ -229,16 +278,19 @@ export default function LoggerPage() {
             });
           }}
           style={{
-            marginRight: '0.5rem',
-            float: 'right',
-            width: '5rem',
-          }} placeholder='模型名称' />
+            marginRight: "0.5rem",
+            float: "right",
+            width: "5rem",
+          }}
+          placeholder="模型名称"
+        />
         <Select
           style={{
-            marginRight: '0.5rem',
-            float: 'right',
-            width: '5rem',
-          }} value={input.type}
+            marginRight: "0.5rem",
+            float: "right",
+            width: "5rem",
+          }}
+          value={input.type}
           onChange={(e: any) => {
             setInput({
               ...input,
@@ -260,10 +312,12 @@ export default function LoggerPage() {
             });
           }}
           style={{
-            marginRight: '0.5rem',
-            float: 'right',
-            width: '5rem',
-          }} placeholder='关键字' />
+            marginRight: "0.5rem",
+            float: "right",
+            width: "5rem",
+          }}
+          placeholder="关键字"
+        />
 
         <DatePicker
           value={input.startTime}
@@ -273,12 +327,13 @@ export default function LoggerPage() {
               startTime: e,
             });
           }}
-
           style={{
-            marginRight: '0.5rem',
-            width: '10rem',
-            float: 'right',
-          }} placeholder='开始时间' />
+            marginRight: "0.5rem",
+            width: "10rem",
+            float: "right",
+          }}
+          placeholder="开始时间"
+        />
         <DatePicker
           value={input.endTime}
           onChange={(e: any) => {
@@ -288,22 +343,27 @@ export default function LoggerPage() {
             });
           }}
           style={{
-            marginRight: '0.5rem',
-            width: '10rem',
-            float: 'right',
-          }} placeholder='结束时间' />
-
+            marginRight: "0.5rem",
+            width: "10rem",
+            float: "right",
+          }}
+          placeholder="结束时间"
+        />
       </Header>
       <Table
         scroll={{
           y: 800,
-          x: 800,
+          x: 1800,
         }}
-        loading={loading} style={{
-          marginTop: '1rem',
-        }} columns={columns
-          .filter((item) => item.disable == false || item.disable == undefined)
-        } dataSource={data} pagination={{
+        loading={loading}
+        style={{
+          marginTop: "1rem",
+        }}
+        columns={columns.filter(
+          (item) => item.disable == false || item.disable == undefined
+        )}
+        dataSource={data}
+        pagination={{
           total: total,
           pageSize: input.pageSize,
           defaultPageSize: input.page,
@@ -315,8 +375,8 @@ export default function LoggerPage() {
               pageSize,
             });
           },
-
-        }} />
+        }}
+      />
     </div>
-  )
+  );
 }
