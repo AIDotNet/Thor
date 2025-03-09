bde90cdc255ba06fe75511e3e96ab6103a1b86f8
token <239573049@qq.com>
2025-03-09 12:29:10 +0800

    使用新版本

--- src/extensions/Thor.AzureOpenAI/AzureOpenAIFactory.cs
+++ src/extensions/Thor.AzureOpenAI/AzureOpenAIFactory.cs
@@ -12,11 +12,7 @@ public static class AzureOpenAIFactory
 
     public static string GetAddress(ThorPlatformOptions options, string model)
     {
-        if (string.IsNullOrEmpty(options.Other))
-        {
-            options.Other = "2024-08-01-preview";
-        }
-        
+        options.Other = "2024-12-01-preview";
         return string.Format(AddressTemplate, options.Address.TrimEnd('/'), model, options.Other);
     }
 
