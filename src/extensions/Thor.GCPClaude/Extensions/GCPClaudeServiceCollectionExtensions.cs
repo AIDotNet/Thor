6a0215d0628f867bb397d49b0df427ea1acf3e79
赵畅畅 <15538136734@163.com>
2025-03-17 01:12:44 +0800

    谷歌Claude

---
+++ src/extensions/Thor.GCPClaude/Extensions/GCPClaudeServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Thor.Abstractions.Chats;
+using Thor.Abstractions;
+using Thor.GoogleClaude;
+using Thor.GCPClaude.Chats;
+
+namespace Thor.GCPClaude.Extensions
+{
+    public static class GCPClaudeServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGCPClaudeService(this IServiceCollection services)
+        {
+            ThorGlobal.PlatformNames.Add(GCPClaudePlatformOptions.PlatformName, GCPClaudePlatformOptions.PlatformCode);
+
+            services.AddKeyedSingleton<IThorChatCompletionsService, GCPClaudeChatCompletionsService>(GCPClaudePlatformOptions.PlatformCode);
+            return services;
+        }
+    }
+}
---
+++ src/extensions/Thor.GCPClaude/GCPClaudePlatformOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Thor.GoogleClaude
+{
+    public class GCPClaudePlatformOptions
+    {
+        /// <summary>
+        /// 平台名称
+        /// </summary>
+        public const string PlatformName = "GCPClaude";
+
+        /// <summary>
+        /// 平台编码
+        /// </summary>
+        public const string PlatformCode = "GCPClaude";
+    }
+}
